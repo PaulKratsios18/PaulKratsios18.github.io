3887cd99f0b1392eceaadcb8aa5e194fc71d96ed
Paul Kratsios <103073643+PaulKratsios18@users.noreply.github.com>
2025-01-12 23:16:31 -0500

    build updates

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import ProjectCard from './ProjectCard.tsx';
-import { title } from 'process';
 
 const Projects = () => {
   const projects = [
